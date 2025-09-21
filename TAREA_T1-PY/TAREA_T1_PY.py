19f2cb6f62ccaf811b7be70a4d3f4a76ea3bf6bb
Eduardo <eduardopuescas5@gmail.com>
2025-09-21 17:10:53 -0500

    Subir proyecto en C# y Python

---
+++ TAREA_T1-PY/TAREA_T1_PY.py
@@ -0,0 +1,84 @@
+﻿import math
+import sys
+
+# Ejercicio 1
+def ejer1():
+    # Declaramos variables
+    precio = 0.0
+    descuento = 0.0
+    precio_final = 0.0
+    codigo = 0
+
+    precio = float(input("Ingrese el precio del producto: "))
+    codigo = int(input("Ingrese el código de descuento (1=Estudiante, 2=Empleado, 3=Cliente frecuente): "))
+
+    # Cantidad de descuento
+    if codigo == 1:
+        descuento = 0.10
+    elif codigo == 2:
+        descuento = 0.15
+    elif codigo == 3:
+        descuento = 0.20
+    else:
+        print("Código inválido, no se aplica descuento.")
+        descuento = 0.0
+
+    # Aplicar descuento
+    precio_final = precio - (precio * descuento)
+
+    # Descuento adicional 
+    if precio > 500:
+        precio_final = precio_final - (precio_final * 0.05)
+
+    print(f"El precio final es: {precio_final:.2f}")
+
+
+# Ejercicio 2
+def ejer2():
+    # Declaramos Variables
+    nota = 0
+    clasificacion = ""
+
+    nota = int(input("Ingrese una nota (0-20): "))
+
+   
+    if nota < 0 or nota > 20:
+        print("Ingrese una nota valida.")
+        sys.exit() 
+
+    # Clasificamos
+    if 0 <= nota <= 10:
+        clasificacion = "Desaprobado"
+    elif 11 <= nota <= 13:
+        clasificacion = "Regular"
+    elif 14 <= nota <= 17:
+        clasificacion = "Bueno"
+    else:  # 18-20
+        clasificacion = "Excelente"
+
+    print(f"Clasificación: {clasificacion}")
+
+    # Condicionales
+    if nota >= 14 and nota % 2 == 0:
+        print("Buen desempeño con nota par")
+    if nota % 2 != 0 or nota <= 11:
+        print("Necesita reforzar")
+
+    # Cálculos
+    raiz = math.sqrt(nota)
+    cubo = math.pow(nota, 3)
+
+    print(f"Raíz cuadrada: {raiz:.1f}")
+    print(f"Cubo de la nota: {cubo}")
+
+def main():
+    ejer1()
+    input("\nPresione una tecla para continuar...")
+
+    ejer2()
+    input("\nPresione una tecla para salir...")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ TAREA_T1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAREA_T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                
+                ejer1();
+                Console.ReadKey();
+
+                ejer2();
+                Console.ReadKey();
+            }
+
+            // Ejercicio 1 
+            static void ejer1()
+            {
+                // Declaramos Variables
+                double precio = 0.0;
+                double descuento = 0.0;
+                double precioFinal = 0.0;
+                int codigo = 0;
+
+                Console.Write("Ingrese el precio del producto: ");
+                precio = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el código de descuento (1=Estudiante, 2=Empleado, 3=Cliente frecuente): ");
+                codigo = int.Parse(Console.ReadLine());
+
+                //Descuento
+                switch (codigo)
+                {
+                    case 1:
+                        descuento = 0.10;
+                        break;
+                    case 2:
+                        descuento = 0.15;
+                        break;
+                    case 3:
+                        descuento = 0.20;
+                        break;
+                    default:
+                        Console.WriteLine("Código inválido, no se aplica descuento.");
+                        descuento = 0.0;
+                        break;
+                }
+
+                // Aplicar descuento
+                precioFinal = precio - (precio * descuento);
+
+                // Descuento adicional 
+                if (precio > 500)
+                {
+                    precioFinal = precioFinal - (precioFinal * 0.05);
+                }
+
+                Console.WriteLine("El precio final es: " + precioFinal.ToString("F2"));
+            }
+
+            // Ejercicio 2
+            static void ejer2()
+            {
+                // Declaramos Variables
+                int nota = 0;
+                string clasificacion = "";
+
+                Console.Write("Ingrese una nota (0-20): ");
+                
+                nota = int.Parse(Console.ReadLine());
+                if (nota < 0 || nota > 20)
+                {
+                    Console.WriteLine("Ingrese una nota valida.");
+                    Environment.Exit(0);
+                }
+
+                // Clasificamos
+                if (nota >= 0 && nota <= 10)
+                {
+                    clasificacion = "Desaprobado";
+                }
+                else if (nota >= 11 && nota <= 13)
+                {
+                    clasificacion = "Regular";
+                }
+                else if (nota >= 14 && nota <= 17)
+                {
+                    clasificacion = "Bueno";
+                }
+                else // 18-20
+                {
+                    clasificacion = "Excelente";
+                }
+
+                Console.WriteLine("Clasificación: " + clasificacion);
+
+                // Condicionales 
+                if (nota >= 14 && nota % 2 == 0)
+                {
+                    Console.WriteLine("Buen desempeño con nota par");
+                }
+
+                if (nota % 2 != 0 || nota <= 11)
+                {
+                    Console.WriteLine("Necesita reforzar");
+                }
+
+                // Cálculos
+                double raiz = Math.Sqrt(nota);
+                double cubo = Math.Pow(nota, 3);
+
+                Console.WriteLine("Raíz cuadrada: " + raiz.ToString("0.0"));
+                Console.WriteLine("Cubo de la nota: " + cubo.ToString());
+            }
+}
+    }
+
